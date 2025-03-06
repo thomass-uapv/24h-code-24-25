87958966732e01a5d53c614aafb1c5f87383def4
Thomas S. <thomas.sanchez@alumni.univ-avignon.fr>
2025-03-06 16:26:24 +0100

    Ajout bordure map

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
+import random
+import time
 import pyxel
 
 class App:
     def __init__(self):
-        self.test = 3
 
-        # self.rat1 = Rat()
-        self.groupe_rat = OrdreRat()
+        self.max_rat = 4 # Nombre maximum de rats qui peuvent apparaitre sur la map
+        self.liste_rat_map = [] # Liste des rats sur la map
+        self.joueur = Rat(is_player=True) # Le joueur
 
         pyxel.init(200, 200, title="Hello Pyxel", fps=60, display_scale=2)
         pyxel.load("test.pyxres")
@@ -13,35 +15,48 @@ class App:
 
     
     def update(self):
-        pass
-        # self.rat1.update()
-        self.groupe_rat.update()
+        self.joueur.update()
+        # randint(0,50) == 0 permet de faire apparaitre un rat sur la map avec une probabilité de 1/50
+        if random.randint(0,50) == 0 and len(self.liste_rat_map) < self.max_rat:
+            self.liste_rat_map.append(Rat(random.randint(0,pyxel.width),2)) # Les rats apparaitront en haut de l'écran
 
     def draw(self):
         pyxel.cls(0)
-        # self.rat1.draw()
-        self.groupe_rat.draw()
+        self.joueur.draw()
+        for rat in self.liste_rat_map:
+            rat.draw()
 
 class Rat:
-    def __init__(self, x=50, y=50):
+    def __init__(self, x=50, y=50, is_player=False):
         self.x = x
         self.y = y
         self.img = 0
         self.sprite = (8,0,8,8)
+        self.is_player = is_player
 
     def mouvement(self):
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            self.x += 3
-            self.x = self.x % pyxel.width
-        if pyxel.btn(pyxel.KEY_LEFT):
-            self.x -= 3
-            self.x = self.x % pyxel.width
-        if pyxel.btn(pyxel.KEY_UP):
-            self.y -= 3
-            self.y = self.y % pyxel.height
-        if pyxel.btn(pyxel.KEY_DOWN):
-            self.y += 3
-            self.y = self.y % pyxel.height
+        if self.is_player:
+            if pyxel.btn(pyxel.KEY_RIGHT):
+                self.x += 3
+            if pyxel.btn(pyxel.KEY_LEFT):
+                self.x -= 3
+            if pyxel.btn(pyxel.KEY_UP):
+                self.y -= 3
+            if pyxel.btn(pyxel.KEY_DOWN):
+                self.y += 3
+        else:
+            pass
+        self.blocage()
+
+    def blocage(self):
+        if self.x < 0:
+            self.x = 0
+        elif self.x > pyxel.width:
+            self.x = pyxel.width-8
+        if self.y < 0:
+            self.y = 0
+        elif self.y > pyxel.height:
+            self.y = pyxel.height-8
 
     def getX(self):
         return self.x
@@ -60,15 +75,22 @@ class Rat:
     
 
 class OrdreRat:
-    def __init__(self):
-        self.liste_rats = [Rat(20+i*5,25+i*5) for i in range(5)]
+    def __init__(self, x=50, y=50):
+        self.joueur = Rat(x,y)
+        self.liste_rats = []
+        # Rat(20+i*5,25+i*15) for i in range(5)
 
     def draw(self):
+        self.joueur.draw()
         for rat in self.liste_rats:
             rat.draw()
 
     def update(self):
+        self.joueur.update()
         for rat in self.liste_rats:
             rat.update()
+
+    def ajout_rat(self):
+        self.liste_rats.append(Rat())
 
 App()
