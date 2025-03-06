42604225d15fe7525d6246e7d5649172153550ca
Thomas S. <thomas.sanchez@alumni.univ-avignon.fr>
2025-03-06 15:51:10 +0100

    ajout groupe de rat

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ import pyxel
 
 class App:
     def __init__(self):
-        self.rat1 = Rat()
+        self.test = 3
+
+        # self.rat1 = Rat()
+        self.groupe_rat = OrdreRat()
 
         pyxel.init(200, 200, title="Hello Pyxel", fps=60, display_scale=2)
         pyxel.load("test.pyxres")
@@ -10,32 +13,35 @@ class App:
 
     
     def update(self):
-        self.rat1.mouvement()
+        pass
+        # self.rat1.update()
+        self.groupe_rat.update()
 
     def draw(self):
         pyxel.cls(0)
-        # Récupérer les coordonnées du rat
-        rat_x = self.rat1.getX()
-        rat_y = self.rat1.getY()
-        rat_metadata = self.rat1.getMetadata()
-        pyxel.blt(rat_x, rat_y, rat_metadata[0], rat_metadata[1][0], rat_metadata[1][1], rat_metadata[1][2], rat_metadata[1][3])
+        # self.rat1.draw()
+        self.groupe_rat.draw()
 
 class Rat:
-    def __init__(self):
-        self.x = 50
-        self.y = 50
+    def __init__(self, x=50, y=50):
+        self.x = x
+        self.y = y
         self.img = 0
         self.sprite = (8,0,8,8)
 
     def mouvement(self):
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.x += 3
+            self.x = self.x % pyxel.width
         if pyxel.btn(pyxel.KEY_LEFT):
             self.x -= 3
+            self.x = self.x % pyxel.width
         if pyxel.btn(pyxel.KEY_UP):
             self.y -= 3
+            self.y = self.y % pyxel.height
         if pyxel.btn(pyxel.KEY_DOWN):
             self.y += 3
+            self.y = self.y % pyxel.height
 
     def getX(self):
         return self.x
@@ -45,5 +51,24 @@ class Rat:
     
     def getMetadata(self):
         return self.img, self.sprite
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, self.img, self.sprite[0], self.sprite[1], self.sprite[2], self.sprite[3])
+
+    def update(self):
+        self.mouvement()
+    
+
+class OrdreRat:
+    def __init__(self):
+        self.liste_rats = [Rat(20+i*5,25+i*5) for i in range(5)]
+
+    def draw(self):
+        for rat in self.liste_rats:
+            rat.draw()
+
+    def update(self):
+        for rat in self.liste_rats:
+            rat.update()
 
 App()
